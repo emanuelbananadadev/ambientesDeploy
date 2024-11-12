a73917c06a8e8665ed9d56f8212281df38ef8480
LucasBrandao934 <lucasemanuel1234@hotmail.com>
2024-11-11 22:32:58 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#pokemon')
+const divTeste = document.querySelector('.teste')
+
+btn.addEventListener('click', ()=> {
+    divTeste.innerHTML = `<img src="https://th.bing.com/th/id/OIP.mtupwKQy9UabTdHvByomDQHaEo?rs=1&pid=ImgDetMain" alt="">
+    `
+})
