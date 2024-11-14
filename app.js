f7e1aba1de6e39361719efe1c8f115713b349430
LucasBrandao934 <lucasemanuel1234@hotmail.com>
2024-11-13 21:26:11 -0300

    Modificacoes no css

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const btn = document.querySelector('#pokemon')
 const divTeste = document.querySelector('.teste')
 
 btn.addEventListener('click', ()=> {
-    divTeste.innerHTML = `<img src="https://th.bing.com/th/id/OIP.mtupwKQy9UabTdHvByomDQHaEo?rs=1&pid=ImgDetMain" alt="">
+    divTeste.innerHTML = `
+    <div class="js">
+    <h1>idiota</h1>
+    <img src="https://th.bing.com/th/id/OIP.mtupwKQy9UabTdHvByomDQHaEo?rs=1&pid=ImgDetMain" alt="">
+    </div>                    
     `
 })
